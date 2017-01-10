f89d594874633198dca505ac89eec0b8be41f500
Double <cpt94@iii.org.tw>
2017-01-10 12:15:35 +0800

    Fixed query-url-using-requests

--- query-url-using-requests/main.py
+++ query-url-using-requests/main.py
@@ -6,15 +6,21 @@ r = requests.get('http://httpbin.org/get', params={'show_env': '1'})
 
 print("---url-----------------------------")
 print(r.url)
+
 print("---status code---------------------")
 print(r.status_code)
+
 print("---response header-----------------")
 print(r.headers)
+
 print("---binary response content---------")
 print(r.content)
+
 print("---response content encoding-------")
 print(r.encoding)
+
 print("---response content----------------")
 print(r.text)
+
 print("---json format response content----")
 print(r.json())
