2094df247f01a12b21c3d30ff692382ba71a5234
Double <cpt94@iii.org.tw>
2017-01-10 15:22:09 +0800

    Added query-url-using-urllib

---
+++ query-url-using-urllib/README.md
@@ -0,0 +1,9 @@
+# Reference
+
+[1] https://docs.python.org/3.5/library/urllib.html
+
+[2] https://docs.python.org/3.5/library/http.client.html
+
+[3] https://docs.python.org/3.5/library/email.message.html#email.message.Message
+
+[4] https://docs.python.org/3.5/library/json.html
---
+++ query-url-using-urllib/main.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import json
+
+from urllib.error import HTTPError
+from urllib.request import Request, urlopen
+from urllib.parse import urlencode
+
+req = Request("http://httpbin.org/get?%s" % urlencode({'show_env': '1'}))
+
+print("---url-----------------------------")
+print(req.get_full_url())
+
+print("---HTTP METHOD---------------------")
+print(req.get_method())
+
+try:
+    rsp = urlopen(req)
+except HTTPError as e:
+    print(e)
+    exit(1)
+
+httpMsg = rsp.info()
+rspContent = rsp.read()
+
+print("---url-----------------------------")
+print(rsp.url)
+
+print("---status code---------------------")
+print(rsp.getcode())
+
+print("---response header-----------------")
+print(httpMsg.items())
+
+print("---binary response content---------")
+print(rspContent)
+
+print("---response content encoding-------")
+print(httpMsg.get_content_charset())
+
+print("---response content----------------")
+print(rspContent.decode('utf-8'))
+
+print("---json format response content----")
+print(json.loads(rspContent.decode('utf-8')))
