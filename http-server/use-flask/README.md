19c3830ae0242c65fa9017c941a7d8f5a689446e
Double <cpt94@iii.org.tw>
2017-01-13 13:29:04 +0800

    Added use-flask

---
+++ http-server/use-flask/README.md
@@ -0,0 +1,7 @@
+# Dependency packages
+
+pip3 install flask
+
+# Reference
+
+[1] https://docs.python.org/3.5/library/http.server.html
---
+++ http-server/use-flask/main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from flask import Flask, jsonify, make_response, request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def my_get_handler():
+    print("PATH: " + request.query_string.decode("utf-8"))
+
+    return make_response(
+        jsonify(
+            {
+                'id': 'XXX-XXXX',
+                'content': 'TBD'
+            }
+        ),
+        200
+    )
+
+if __name__ == '__main__':
+    app.run(
+        host='127.0.0.1',
+        port=8000,
+        threaded=True,
+        debug=True
+    )
