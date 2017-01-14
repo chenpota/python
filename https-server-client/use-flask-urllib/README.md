2bc015829afc584c387295bf034c0a478b27a9d7
double <double@double-pc.cm>
2017-01-14 11:39:33 +0800

    Added https-server-client/use-flask-urllib

---
+++ https-server-client/use-flask-urllib/README.md
@@ -0,0 +1,16 @@
+# Dependency packages
+
+pip3 install flask
+
+# Create certificate files
+
+openssl req -new -x509 -keyout server.key -out server.pem -days 365 -nodes -subj '/C=TW/ST=Taiwan/L=Taipei/CN=127.0.0.1/emailAddress=test@test.com'
+
+openssl x509 -outform pem -in server.pem -out mycert.crt
+
+# curl
+curl -i --cacert mycert.crt https://127.0.0.1:8000
+
+# Reference
+
+[1] https://docs.python.org/3.5/library/http.server.html
---
+++ https-server-client/use-flask-urllib/client.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import json
+import ssl
+import urllib.error
+import urllib.parse
+import urllib.request
+
+
+req = urllib.request.Request("https://127.0.0.1:8000/")
+
+ctx = ssl.create_default_context(cafile='mycert.crt')
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_REQUIRED
+
+try:
+    rsp = urllib.request.urlopen(req, context=ctx)
+except urllib.error.HTTPError as e:
+    print(e)
+    exit(1)
+
+print("---status code---------------------")
+print(rsp.getcode())
+
+print("---response header-----------------")
+print(rsp.info())
+
+print("---binary response content---------")
+print(rsp.read())
+
+rsp.close()
