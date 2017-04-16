860fe30b85b192044c76433fd55539e8fa062734
double <double@double-pc.cm>
2017-04-16 21:15:13 +0800

    Fix coding style

--- http-server/use-flask-blueprint/test/test_app.py
+++ http-server/use-flask-blueprint/test/test_app.py
@@ -7,22 +7,22 @@ from app import create_server
 class TestApp(unittest.TestCase):
 
     def setUp(self):
-        self.server = create_server()
-        self.server_ctx = self.server.app_context()
-        self.server_ctx.push()
-        self.client = self.server.test_client()
+        self.app = create_server()
+        self.app_ctx = self.app.app_context()
+        self.app_ctx.push()
+        self.client = self.app.test_client()
 
     def tearDown(self):
-        self.server_ctx.pop()
+        self.app_ctx.pop()
 
     def test_teacher(self):
         rsp = self.client.get('teacher')
-        self.assertEqual(rsp.data.decode('utf-8'), 'teacher')
+        self.assertEqual(rsp.get_data(as_text=True), 'teacher')
         self.assertEqual(rsp.status_code, 200)
 
     def test_student(self):
         rsp = self.client.get('student')
-        self.assertEqual(rsp.data.decode('utf-8'), 'student')
+        self.assertEqual(rsp.get_data(as_text=True), 'student')
         self.assertEqual(rsp.status_code, 200)
 
     def test_strenger(self):
