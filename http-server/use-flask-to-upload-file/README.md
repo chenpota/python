ad506403fa1ecc18933f5215c873f4b9812434c3
chenpota <double1217@gmail.com>
2017-05-09 16:32:11 +0800

    Add upload file via Flask

---
+++ http-server/use-flask-to-upload-file/README.md
@@ -0,0 +1,7 @@
+# Dependency packages
+
+pip3 install flask
+
+# Reference
+
+[1] http://flask.pocoo.org/docs/0.12/patterns/fileuploads/
---
+++ http-server/use-flask-to-upload-file/main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import os
+from flask import Flask, redirect, render_template, request
+from werkzeug.utils import secure_filename
+
+
+APP = Flask(__name__)
+
+
+@APP.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'GET':
+        return render_template('choose_file.html')
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return redirect(request.url)
+
+    file.save(os.path.join('./', file.filename))
+    return "Upload {0} successfully".format(file.filename), 200
+
+
+if __name__ == '__main__':
+    APP.run(
+        host='127.0.0.1',
+        port=8000
+    )
