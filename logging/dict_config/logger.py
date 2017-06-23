c11259110a43757acbdb8009eabe65157b21ae5f
chenpota <double1217@gmail.com>
2017-06-23 14:45:57 +0800

    Add syslog

--- logging/dict_config/logger.py
+++ logging/dict_config/logger.py
@@ -2,26 +2,35 @@ CONFIG = {
     'version': 1,
     'disable_existing_loggers': False,
     'formatters': {
-        'default': {
+        'stderr': {
             'format': '%(asctime)s - %(thread)d - %(name)s - %(levelname)s: %(message)s'
+        },
+        'syslog': {
+            'format': '%(thread)d - %(name)s - %(levelname)s: %(message)s'
         }
     },
     'handlers': {
-        'console': {
+        'stderr': {
             'level': 'DEBUG',
             'class': 'logging.StreamHandler',
-            'formatter': 'default',
-            'stream': 'ext://sys.stderr'
+            'formatter': 'stderr'
+        },
+        'syslog': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.SysLogHandler',
+            'formatter': 'syslog',
+            'address': ('localhost', 514)
+            #'address': '/dev/log'
         }
     },
     'loggers': {
         'dict_config.app': {
             'level': 'DEBUG',
-            'handlers': ['console']
+            'handlers': ['stderr', 'syslog']
         },
         'dict_config.module.library': {
             'level': 'DEBUG',
-            'handlers': ['console']
+            'handlers': ['stderr', 'syslog']
         }
     }
 }
