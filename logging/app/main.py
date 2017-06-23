fdf1a22886f3b02ea40bed9d1ab5390a5ae8dfa8
chenpota <double1217@gmail.com>
2017-06-23 17:13:25 +0800

    Add yaml

--- logging/app/main.py
+++ logging/app/main.py
@@ -2,9 +2,19 @@
 
 import logging
 import logging.config
+import os
+
+import yaml
 
 from app import library
 from app import logger
+
+
+def load_logger_yaml():
+    logger_cfg_path = os.path.dirname(__file__) + '/logger.yaml'
+
+    with open(logger_cfg_path) as file_desc:
+        return yaml.load(file_desc.read())
 
 
 def main():
@@ -18,6 +28,8 @@ def main():
 
 
 if __name__ == '__main__':
-    logging.config.dictConfig(logger.CONFIG)
+
+    # logging.config.dictConfig(logger.CONFIG)
+    logging.config.dictConfig(load_logger_yaml())
 
     main()
