790b013e9c62d03112ab96e5d14c9f1903bb504b
PoTa Chen <double1217@gmail.com>
2017-06-29 12:03:42 +0800

    Fix name

--- type-hints/example/06_none.py
+++ type-hints/example/06_none.py
@@ -1,20 +1,20 @@
 from typing import Optional, Union
 
 
-def func_pass_int_or_none_v1(v: int) -> None:
+def func_pass_int(v: int) -> None:
     pass
 
 
-def func_pass_int_or_none_v2(v: Optional[int]) -> None:  # perfered format
+def func_pass_int_or_none_v1(v: Optional[int]) -> None:  # perfered format
     pass
 
 
-def func_pass_int_or_none_v3(v: Union[int, None]) -> None:
+def func_pass_int_or_none_v2(v: Union[int, None]) -> None:
     pass
 
-func_pass_int_or_none_v1(None)  # type error if mypy --strict-optional
+func_pass_int(None)  # type error if mypy --strict-optional
+func_pass_int_or_none_v1(None)
 func_pass_int_or_none_v2(None)
-func_pass_int_or_none_v3(None)
 
 
 def func_with_default_args_v1(v: int = None) -> None:  # perfered format
@@ -31,13 +31,13 @@ func_with_default_args_v2()
 func_with_default_args_v2(None)
 
 
-def func_return_int_or_none_v1() -> int:
+def func_return_int() -> int:
     return None  # type error if mypy --strict-optional
 
 
-def func_return_int_or_none_v2() -> Optional[int]:  # perfered format
+def func_return_int_or_none_v1() -> Optional[int]:  # perfered format
     return None
 
 
-def func_return_int_or_none_v3() -> Union[int, None]:
+def func_return_int_or_none_v2() -> Union[int, None]:
     return None
